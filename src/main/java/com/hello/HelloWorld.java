d322e07a0e3bd8c504ed9852e8f514f19a7d71d8
sridharkondeti <sridharreddy2kx@gmail.com>
2022-12-16 10:52:27 -0600

    Update HelloWorld.java

--- src/main/java/com/hello/HelloWorld.java
+++ src/main/java/com/hello/HelloWorld.java
@@ -4,4 +4,7 @@ public class HelloWorld {
   public void hello() {
     return "This is the first Hello World Web Application using Tomcat/Servlet!!! - 1";
   }
+  public void hello_1() {
+    return "Test"
+  }
 }
