f9bd2c4ab204979bfe2e7a3f062ded86e6bb236e
sridharkondeti <sridharreddy2kx@gmail.com>
2022-12-16 10:51:37 -0600

    Create HelloWorldTest.java

---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,10 @@
+import com.hello.HelloWorld;
+import org.junit.Test;
+
+public class HelloWorldTest {
+    @Test
+    public void hello_test() {
+        HelloWorld hw = new HelloWorld();
+        hw.hello();
+    }
+}
